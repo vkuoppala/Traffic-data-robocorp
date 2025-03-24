d2d6d17953a199b925f38990aa62fe019b453b08
Ville Kuoppala <ville.kuoppala@riverit.fi>
2025-03-24 12:31:59 +0200

    added log prints

--- orchestrator.py
+++ orchestrator.py
@@ -2,11 +2,11 @@ from prefect import flow, task
 from producer import produce_traffic_data
 from consumer import consume_traffic_data
 
-@task
+@task(log_prints=True)
 def generate_work_items():
     return produce_traffic_data()  # Returns list of work items (JSON)
 
-@task
+@task(log_prints=True)
 def process_work_items(work_items):
     consume_traffic_data(work_items)  # In-memory passing
 
